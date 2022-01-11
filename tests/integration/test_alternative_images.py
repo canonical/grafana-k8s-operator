fa16f1af96534dc39dbd646f54b0260dedc36798
sed-i <82407168+sed-i@users.noreply.github.com>
2022-01-11 00:56:39 -0500

    split main itest into multiple files

---
+++ tests/integration/test_alternative_images.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import pytest
+from helpers import check_prometheus_is_ready, oci_image
+
+prometheus_resources = {"prometheus-image": oci_image("./metadata.yaml", "prometheus-image")}
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy_with_alternative_images(ops_test, grafana_charm):
+    """Test that the Prometheus charm can be deployed successfully."""
+    app_name = "prometheus-ubuntu"
+
+    await ops_test.model.deploy(
+        grafana_charm, resources=prometheus_resources, application_name=app_name
+    )
+    await ops_test.model.wait_for_idle(apps=[app_name], status="active")
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications[app_name].units) > 0)
+
+    assert ops_test.model.applications[app_name].units[0].workload_status == "active"
+
+    await check_prometheus_is_ready(ops_test, app_name, 0)
+
+    await ops_test.model.applications[app_name].remove()
+    await ops_test.model.block_until(lambda: app_name not in ops_test.model.applications)
+    await ops_test.model.reset()
--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -16,45 +16,12 @@ from helpers import (
 
 logger = logging.getLogger(__name__)
 
-
-@pytest.mark.abort_on_fail
-async def test_build_and_deploy_with_alternative_images(ops_test, grafana_charm):
-    """Test that the Grafana charm can be deployed successfully."""
-    resources = {"grafana-image": oci_image("./metadata.yaml", "grafana-image")}
-    app_name = "grafana-ubuntu"
-
-    await ops_test.model.deploy(grafana_charm, resources=resources, application_name=app_name)
-    await ops_test.model.wait_for_idle(apps=[app_name], status="active")
-    await ops_test.model.block_until(lambda: len(ops_test.model.applications[app_name].units) > 0)
-
-    assert ops_test.model.applications[app_name].units[0].workload_status == "active"
-
-    await check_grafana_is_ready(ops_test, app_name, 0)
-
-    await ops_test.model.applications[app_name].remove()
-    await ops_test.model.reset()
-
-
-@pytest.mark.abort_on_fail
-async def test_build_and_deploy_grafana_tester(ops_test, grafana_tester_charm):
-    """Test that Grafana tester charm can be deployed successfully."""
-    resources = {
-        "grafana-tester-image": oci_image(
-            "./tests/integration/grafana-tester/metadata.yaml", "grafana-tester-image"
-        )
-    }
-    app_name = "grafana-tester"
-
-    await ops_test.model.deploy(
-        grafana_tester_charm, resources=resources, application_name=app_name
+tester_resources = {
+    "grafana-tester-image": oci_image(
+        "./tests/integration/grafana-tester/metadata.yaml", "grafana-tester-image"
     )
-    await ops_test.model.wait_for_idle(apps=[app_name], status="active")
-    await ops_test.model.block_until(lambda: len(ops_test.model.applications[app_name].units) > 0)
-
-    assert ops_test.model.applications[app_name].units[0].workload_status == "active"
-
-    await ops_test.model.applications[app_name].remove()
-    await ops_test.model.reset()
+}
+grafana_resources = {"grafana-image": oci_image("./metadata.yaml", "grafana-image")}
 
 
 @pytest.mark.abort_on_fail
@@ -62,12 +29,6 @@ async def test_grafana_source_relation_data_with_grafana_tester(
     ops_test, grafana_charm, grafana_tester_charm
 ):
     """Test basic functionality of grafana-source relation interface."""
-    tester_resources = {
-        "grafana-tester-image": oci_image(
-            "./tests/integration/grafana-tester/metadata.yaml", "grafana-tester-image"
-        )
-    }
-    grafana_resources = {"grafana-image": oci_image("./metadata.yaml", "grafana-image")}
     grafana_app_name = "grafana"
     tester_app_name = "grafana-tester"
 
@@ -118,12 +79,6 @@ async def test_grafana_dashboard_relation_data_with_grafana_tester(
     ops_test, grafana_charm, grafana_tester_charm
 ):
     """Test basic functionality of grafana-dashboard relation interface."""
-    tester_resources = {
-        "grafana-tester-image": oci_image(
-            "./tests/integration/grafana-tester/metadata.yaml", "grafana-tester-image"
-        )
-    }
-    grafana_resources = {"grafana-image": oci_image("./metadata.yaml", "grafana-image")}
     grafana_app_name = "grafana"
     tester_app_name = "grafana-tester"
 
