10c2861b8cb6c62572a156500e62621f10666fec
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2022-08-03 01:45:00 +0000

    enable resource limits

--- src/charm.py
+++ src/charm.py
@@ -35,7 +35,13 @@ from charms.grafana_k8s.v0.grafana_source import (
     GrafanaSourceEvents,
     SourceFieldsMissingError,
 )
+from charms.observability_libs.v0.kubernetes_compute_resources_patch import (
+    K8sResourcePatchFailedEvent,
+    KubernetesComputeResourcesPatch,
+    adjust_resource_requirements,
+)
 from charms.prometheus_k8s.v0.prometheus_scrape import MetricsEndpointProvider
+from lightkube.models.core_v1 import ResourceRequirements
 from ops.charm import (
     ActionEvent,
     CharmBase,
@@ -46,7 +52,7 @@ from ops.charm import (
 )
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus, MaintenanceStatus
+from ops.model import ActiveStatus, MaintenanceStatus, BlockedStatus
 from ops.pebble import ConnectionError, Layer, PathError, ProtocolError
 
 from grafana_server import Grafana
@@ -134,6 +140,12 @@ class GrafanaCharm(CharmBase):
         # -- database relation observations
         self.framework.observe(self.on[DATABASE].relation_changed, self._on_database_changed)
         self.framework.observe(self.on[DATABASE].relation_broken, self._on_database_broken)
+
+        # -- k8s resource patch
+        self.resource_patch = KubernetesComputeResourcesPatch(
+            self, self.name, resource_reqs_func=self._resource_reqs_from_config
+        )
+        self.framework.observe(self.resource_patch.on.patch_failed, self._on_resource_patch_failed)
 
     def _on_install(self, _):
         """Handler for the install event during which we will update the K8s service."""
@@ -697,6 +709,14 @@ class GrafanaCharm(CharmBase):
         # through subprocess. So much for complex password
         chars = string.ascii_letters + string.digits
         return "".join(secrets.choice(chars) for _ in range(12))
+
+    def _resource_reqs_from_config(self) -> ResourceRequirements:
+        limits = {"cpu": self.model.config.get("cpu"), "memory": self.model.config.get("memory")}
+        requests = {"cpu": "0.25", "memory": "200Mi"}
+        return adjust_resource_requirements(limits, requests, adhere_to_requests=True)
+
+    def _on_resource_patch_failed(self, event: K8sResourcePatchFailedEvent):
+        self.unit.status = BlockedStatus(event.message)
 
 
 if __name__ == "__main__":
