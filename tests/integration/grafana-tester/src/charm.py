7d783c3fff116f21fa7e2520f88d3dfb0f2f8c4a
yazansalti <yazan.salti@canonical.com>
2022-09-07 13:40:53 +0200

    Adds a minor fix in grafana-tester charm Fixes when the charm should be in blocked status

--- tests/integration/grafana-tester/src/charm.py
+++ tests/integration/grafana-tester/src/charm.py
@@ -52,7 +52,7 @@ class GrafanaTesterCharm(CharmBase):
         self.unit.status = ActiveStatus()
 
     def _on_urls_available(self, event):
-        if not event.urls:
+        if not self.urls and not event.urls:
             self.unit.status = BlockedStatus("Waiting for grafana urls")
             event.defer()
             return
