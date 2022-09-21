9e1d05dfb1324bb0db9049a134d4007093ceb589
Ryan Barry <ryan.barry@canonical.com>
2022-09-21 01:07:05 +0000

    Reconcile lib versions

--- lib/charms/grafana_k8s/v0/grafana_dashboard.py
+++ lib/charms/grafana_k8s/v0/grafana_dashboard.py
@@ -218,7 +218,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 15
+LIBPATCH = 14
 
 logger = logging.getLogger(__name__)
 
