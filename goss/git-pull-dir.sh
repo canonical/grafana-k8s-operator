138f75a5c94bf0d9e7440c31424e6c46f169c572
Michael Thamm <mike.thamm@canonical.com>
2024-10-03 18:02:43 -0400

    Feat: Add the fetch-dir script

---
+++ goss/git-pull-dir.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+uri="$1"
+repo="$2"
+branch="$3"
+dir="$4"
+
+git clone -b $branch -n --depth=1 --filter=tree:0 "$uri/$repo.git"
+cd $repo
+git sparse-checkout set --no-cone $dir
+git checkout
