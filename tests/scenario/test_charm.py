d2133ae271eb8c50c6eb84f10ee036b25a79e5ab
sed-i <82407168+sed-i@users.noreply.github.com>
2024-11-12 14:31:07 -0500

    Add scenario tests

--- tests/scenario/test_charm.py
+++ tests/scenario/test_charm.py
@@ -1,5 +1,40 @@
-import pytest
-from ops.testing import State, PeerRelation
+from ops.testing import State
+from configparser import ConfigParser
 
-def test_placeholder(ctx):
-    pass
+
+def test_reporting_enabled(ctx):
+    # GIVEN the "reporting_enabled" config option is set to True
+    state = State(leader=True, config={"reporting_enabled": True})
+    # WHEN config-changed fires
+    out = ctx.run(ctx.on.config_changed(), state)
+
+    # THEN the config file is written WITHOUT the [analytics] section being rendered
+    simulated_pebble_filesystem = out.get_container("grafana").get_filesystem(ctx)
+    grafana_config_path = simulated_pebble_filesystem / "etc/grafana/grafana-config.ini"
+
+    config = ConfigParser()
+    config.read(grafana_config_path)
+    assert "analytics" not in config
+
+
+def test_reporting_disabled(ctx):
+    # GIVEN the "reporting_enabled" config option is set to True
+    state = State(leader=True, config={"reporting_enabled": False})
+    # WHEN config-changed fires
+    out = ctx.run(ctx.on.config_changed(), state)
+
+    # THEN the config file is written WITH the [analytics] section being rendered
+    simulated_pebble_filesystem = out.get_container("grafana").get_filesystem(ctx)
+    grafana_config_path = simulated_pebble_filesystem / "etc/grafana/grafana-config.ini"
+
+    config = ConfigParser()
+    config.read(grafana_config_path)
+    assert dict(config["analytics"]) == {
+        'reporting_enabled': 'false',
+        'check_for_updates': 'false',
+        'check_for_plugin_updates': 'false',
+    }
+
+    # AND the "grafana" service is restarted
+    # TODO Does it make sense to check this if the charm under test's lifetime is only for the config-changed?
+    # TODO How to assert this?
