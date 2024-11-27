e821e40ac134468ece69c68ee9bc117b27c5f600
Jose C. Massón <939888+Abuelodelanada@users.noreply.github.com>
2024-11-27 08:34:26 -0300

    Update outputs.tf (#360)
    * Update outputs.tf
    
    Fix endpoint name
    
    Signed-off-by: Jose C. Massón <939888+Abuelodelanada@users.noreply.github.com>
    
    * Update outputs.tf
    
    Signed-off-by: Jose C. Massón <939888+Abuelodelanada@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Jose C. Massón <939888+Abuelodelanada@users.noreply.github.com>

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,7 +14,7 @@ output "endpoints" {
     ingress           = "ingress",
     oauth             = "oauth",
     receive_ca_cert   = "receive-ca-cert",
-    tracing           = "tracing",
+    charm_tracing     = "charm-tracing",
     # Provides
     metrics_endpoint = "metrics-endpoint",
   }
