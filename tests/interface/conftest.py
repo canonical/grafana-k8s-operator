48d52e558a81b363074902b81c26707174aa2bb7
Pietro Pasotti <starfire.daemon@gmail.com>
2024-12-02 10:22:13 +0100

    fmt

--- tests/interface/conftest.py
+++ tests/interface/conftest.py
@@ -2,10 +2,10 @@ from contextlib import ExitStack
 from unittest.mock import patch
 
 import pytest
-from ops.testing import Context
 from scenario import State, Container
 
 from charm import GrafanaCharm
+
 
 @pytest.fixture
 def containers():
@@ -21,16 +21,21 @@ def patches():
     with ExitStack() as stack:
         stack.enter_context(patch("charm.GrafanaCharm._push_sqlite_static", new=lambda _: None))
         stack.enter_context(patch("lightkube.core.client.GenericSyncClient"))
-        stack.enter_context(patch("socket.getfqdn", new=lambda *args: "grafana-k8s-0.testmodel.svc.cluster.local"))
+        stack.enter_context(
+            patch("socket.getfqdn", new=lambda *args: "grafana-k8s-0.testmodel.svc.cluster.local")
+        )
         stack.enter_context(patch("socket.gethostbyname", new=lambda *args: "1.2.3.4"))
-        stack.enter_context(patch.multiple(
-            "charm.KubernetesComputeResourcesPatch",
-            _namespace="test-namespace",
-            _patch=lambda *_a,**_k: True,
-            is_ready=lambda *_a,**_k: True,
-        ))
+        stack.enter_context(
+            patch.multiple(
+                "charm.KubernetesComputeResourcesPatch",
+                _namespace="test-namespace",
+                _patch=lambda *_a, **_k: True,
+                is_ready=lambda *_a, **_k: True,
+            )
+        )
         stack.enter_context(patch.object(GrafanaCharm, "grafana_version", "0.1.0"))
         yield
+
 
 @pytest.fixture
 def grafana_source_tester(interface_tester, containers):
@@ -42,4 +47,3 @@ def grafana_source_tester(interface_tester, containers):
         ),
     )
     yield interface_tester
-
--- tests/scenario/conftest.py
+++ tests/scenario/conftest.py
@@ -3,7 +3,6 @@ from unittest.mock import patch
 
 import pytest
 from ops.testing import Context
-from scenario import Container
 
 from charm import GrafanaCharm
 
@@ -13,15 +12,17 @@ def ctx():
     with ExitStack() as stack:
         stack.enter_context(patch("charm.GrafanaCharm._push_sqlite_static", new=lambda _: None))
         stack.enter_context(patch("lightkube.core.client.GenericSyncClient"))
-        stack.enter_context(patch("socket.getfqdn", new=lambda *args: "grafana-k8s-0.testmodel.svc.cluster.local"))
+        stack.enter_context(
+            patch("socket.getfqdn", new=lambda *args: "grafana-k8s-0.testmodel.svc.cluster.local")
+        )
         stack.enter_context(patch("socket.gethostbyname", new=lambda *args: "1.2.3.4"))
-        stack.enter_context(patch.multiple(
-            "charm.KubernetesComputeResourcesPatch",
-            _namespace="test-namespace",
-            _patch=lambda *_a,**_k: True,
-            is_ready=lambda *_a,**_k: True,
-        ))
+        stack.enter_context(
+            patch.multiple(
+                "charm.KubernetesComputeResourcesPatch",
+                _namespace="test-namespace",
+                _patch=lambda *_a, **_k: True,
+                is_ready=lambda *_a, **_k: True,
+            )
+        )
         stack.enter_context(patch.object(GrafanaCharm, "grafana_version", "0.1.0"))
         yield Context(GrafanaCharm)
-
-
