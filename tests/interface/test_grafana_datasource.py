807686266d58ea07f419769ab6e740cfdac41978
Pietro Pasotti <starfire.daemon@gmail.com>
2024-12-02 14:57:59 +0100

    unpinned CRI branch

--- tests/interface/test_grafana_datasource.py
+++ tests/interface/test_grafana_datasource.py
@@ -4,7 +4,6 @@ from interface_tester import InterfaceTester
 def test_grafana_datasource_v0_interface(grafana_source_tester: InterfaceTester):
     grafana_source_tester.configure(
         interface_name="grafana_datasource",
-        branch="grafana-datasource-interface",
         interface_version=0,
     )
     grafana_source_tester.run()
