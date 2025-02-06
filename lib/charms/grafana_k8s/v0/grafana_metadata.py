1c33e9c5fa553ca8edc50981902890fc4977d642
Andrew Scribner <ca.scribner+1@gmail.com>
2025-02-06 12:12:35 -0500

    docs: add grafana-metadata library docstring

--- lib/charms/grafana_k8s/v0/grafana_metadata.py
+++ lib/charms/grafana_k8s/v0/grafana_metadata.py
@@ -1,22 +1,80 @@
-"""TODO: Add a proper docstring here.
+"""grafana_metadata
 
-This is a placeholder docstring for this charm library. Docstrings are
-presented on Charmhub and updated whenever you push a new version of the
-library.
+This implements provider and requirer sides of the grafana-metadata interface, which is used to communicate information
+about an Grafana installation such as its url and UID.
 
-Complete documentation about creating and documenting libraries can be found
-in the SDK docs at https://juju.is/docs/sdk/libraries.
+## Usage
 
-See `charmcraft publish-lib` and `charmcraft fetch-lib` for details of how to
-share and consume charm libraries. They serve to enhance collaboration
-between charmers. Use a charmer's libraries for classes that handle
-integration with their charm.
+### Requirer
 
-Bear in mind that new revisions of the different major API versions (v0, v1,
-v2 etc) are maintained independently.  You can continue to update v0 and v1
-after you have pushed v3.
+To add this relation to your charm as a requirer, add the following to your `charmcraft.yaml` or `metadata.yaml`:
 
-Markdown is supported, following the CommonMark specification.
+```yaml
+requires:
+  grafana-metadata:
+    # The example below uses the API for when limit=1.  If you need to support multiple related applications, remove
+    # this and use the list-based data accessor method.
+    limit: 1
+    interface: grafana_metadata
+```
+
+To handle the relation events in your charm, use `GrafanaMetadataRequirer`.  That object handles all relation events for
+this relation, and emits a `DataChangedEvent` when data changes the charm might want to react to occur.  To set it up,
+instantiate an `GrafanaMetadataRequirer` object in your charm's `__init__` method and observe the `DataChangedEvent`:
+
+```python
+class FooCharm(CharmBase):
+    def __init__(self, framework):
+        super().__init__(framework)
+        # Create the GrafanaMetadataRequirer instance, providing the relation name you've used
+        self.grafana_metadata = GrafanaMetadataRequirer(self, "grafana-metadata")
+        self.framework.observe(self.grafana_metadata.on.data_changed, self.do_something_with_metadata)
+```
+
+To access the data elsewhere in the charm, use the provided data accessors.  These return `GrafanaMetadataAppData`
+objects:
+
+```python
+class FooCharm(CharmBase):
+    ...
+    # If using limit=1
+    def do_something_with_metadata(self):
+        # Get exactly one related application's data, raising if more than one is available
+        # note: if not using limit=1, see .get_data_from_all_relations()
+        metadata = self.grafana_metadata.get_data()
+        if metadata is None:
+            self.log("No metadata available yet")
+            return
+        self.log(f"Got Grafana's internal_url: {metadata.internal_url}")
+```
+
+### Provider
+
+To add this relation to your charm as a provider, add the following to your `charmcraft.yaml` or `metadata.yaml`:
+
+```yaml
+provides:
+  grafana-metadata:
+    interface: grafana_metadata
+```
+
+To handle the relation events in your charm, use `GrafanaMetadataProvider`.  That object sends data to all related
+requirers automatically when applications join.  To set it up, instantiate an `GrafanaMetadataProvider` object in your
+charm's `__init__` method:
+
+```python
+class FooCharm(CharmBase):
+    def __init__(self, framework):
+        super().__init__(framework)
+        # Create the GrafanaMetadataProvider instance, providing the root namespace for the Grafana installation
+        self.grafana_metadata = GrafanaMetadataProvider(
+            charm=self,
+            grafana_uid=self.unique_name,
+            ingress_url=self.external_url,
+            internal_url=self.internal_url,
+            relation_name="grafana-metadata"
+        )
+```
 """
 
 from typing import List, Optional, Union
@@ -39,6 +97,9 @@ LIBAPI = 0
 # to 0 if you are raising the major API version
 LIBPATCH = 1
 
+# Uncomment once we're actually pulling from this repo instead of a gh branch
+# PYDEPS = ["charm-relation-building-blocks"]
+
 DEFAULT_RELATION_NAME = "grafana-info"
 
 
@@ -48,9 +109,9 @@ class GrafanaMetadataAppData(BaseModel):
     """Data model for the grafana-info interface."""
 
     # TODO: Copy this exactly from Dylan's mimir code
-    ingress_url: str = Field()
-    internal_url: str = Field()
-    grafana_uid: str = Field()
+    ingress_url: str = Field(description="The ingress URL.")
+    internal_url: str = Field(description="The URL for connecting to the prometheus api from inside the cluster.")
+    grafana_uid: str = Field(description="The UID of this Grafana application.")
 
 
 class GrafanaMetadataRequirer(Receiver):
