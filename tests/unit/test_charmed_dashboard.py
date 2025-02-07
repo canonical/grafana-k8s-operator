68d815876fbb63a4c37d9f146283109d3423f71a
sed-i <82407168+sed-i@users.noreply.github.com>
2025-02-07 13:13:38 -0500

    Add tag with charm name to dashboard

---
+++ tests/unit/test_charmed_dashboard.py
@@ -0,0 +1,38 @@
+import unittest
+from charms.grafana_k8s.v0.grafana_dashboard import CharmedDashboard
+
+
+class CharmedDashboardTest(unittest.TestCase):
+    def test_add_tags_to_dashboard_without_tags(self):
+        # GIVEN a dashboard dict with no tags
+        dashboard = {}
+
+        # WHEN tags are added
+        CharmedDashboard._add_tags(dashboard, "my-charm")
+
+        # THEN list of tags only contains MyCharm
+        self.assertListEqual(dashboard["tags"], ["charm: my-charm"])
+
+    def test_add_tags_to_dashboard_with_tags(self):
+        # GIVEN a dashboard dict with some tags
+        dashboard = {"tags": ["one", "two"]}
+
+        # WHEN tags are added
+        CharmedDashboard._add_tags(dashboard, "my-charm")
+
+        # THEN list of tags is extended with MyCharm
+        self.assertListEqual(dashboard["tags"], ["one", "two", "charm: my-charm"])
+
+    def test_add_tags_to_dashboard_with_charm_tag(self):
+        # GIVEN a dashboard dict with a tag that starts with "charm: "
+        dashboard = {"tags": ["charm: something-else"]}
+
+        # WHEN tags are added
+        CharmedDashboard._add_tags(dashboard, "my-charm")
+
+        # THEN list of tags is unaffected
+        self.assertListEqual(dashboard["tags"], ["charm: something-else"])
+
+
+if __name__ == '__main__':
+    unittest.main()
