bd5ad225075206ee6f6a98f4c962675e4005519e
sed-i <82407168+sed-i@users.noreply.github.com>
2025-02-07 13:19:31 -0500

    Bump LIBPATCH

--- lib/charms/grafana_k8s/v0/grafana_dashboard.py
+++ lib/charms/grafana_k8s/v0/grafana_dashboard.py
@@ -219,7 +219,7 @@ LIBAPI = 0
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
 
-LIBPATCH = 41
+LIBPATCH = 42
 
 PYDEPS = ["cosl >= 0.0.50"]
 
