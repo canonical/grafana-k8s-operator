d6640a4637edc68272df7d13f2e7a96fbbb08851
Luca Bello <36242061+lucabello@users.noreply.github.com>
2025-05-23 11:21:31 +0200

    feat: switch to track 2 (#420)
    * feat: preapre for track 2
    
    * fix static
    
    * patch series
    
    * Use sh
    
    * Use sh
    
    * uv lock
    
    * fix static
    
    * update lockfile
    
    * chore: trigger ci
    
    * try to fix itest
    
    ---------
    
    Co-authored-by: Pietro Pasotti <pietro.pasotti@canonical.com>
    Co-authored-by: Leon <82407168+sed-i@users.noreply.github.com>

--- tests/integration/test_trusted_certificates.py
+++ tests/integration/test_trusted_certificates.py
@@ -4,11 +4,13 @@
 
 import logging
 from pathlib import Path
-
+import sh
 import pytest
 import yaml
 from helpers import oci_image
 from pytest_operator.plugin import OpsTest
+
+# pyright: reportAttributeAccessIssue = false
 
 logger = logging.getLogger(__name__)
 
@@ -22,17 +24,15 @@ grafana_resources = {
 @pytest.mark.skip_if_deployed
 @pytest.mark.abort_on_fail
 async def test_deploy(ops_test, grafana_charm):
-    await ops_test.model.deploy(
+    sh.juju.deploy(
         grafana_charm,
-        resources=grafana_resources,
-        application_name="grafana",
+        "grafana",
+        model=ops_test.model.name,
         trust=True,
+        resource=[f"{k}={v}" for k, v in grafana_resources.items()],
     )
-    await ops_test.model.deploy(
-        "self-signed-certificates",
-        application_name="ca",
-        channel="latest/edge",
-        trust=True,
+    sh.juju.deploy(
+        "self-signed-certificates", "ca", model=ops_test.model.name, channel="latest/edge"
     )
 
     await ops_test.model.add_relation("grafana:receive-ca-cert", "ca")
@@ -92,7 +92,8 @@ async def test_certs_created(ops_test: OpsTest):
 async def test_certs_available_after_refresh(ops_test: OpsTest, grafana_charm):
     """Make sure trusted certs are available after update."""
     assert ops_test.model
-    await ops_test.model.applications["grafana"].refresh(path=grafana_charm)  # type: ignore
+    sh.juju.refresh("grafana", model=ops_test.model.name, path=grafana_charm)
+
     await ops_test.model.wait_for_idle(
         status="active", raise_on_error=False, timeout=600, idle_period=30
     )
--- tests/integration/test_upgrade_charm.py
+++ tests/integration/test_upgrade_charm.py
@@ -4,9 +4,11 @@
 
 
 import logging
-
+import sh
 import pytest
 from helpers import check_grafana_is_ready, get_config_values, oci_image
+
+# pyright: reportAttributeAccessIssue = false
 
 logger = logging.getLogger(__name__)
 
@@ -21,18 +23,20 @@ grafana_resources = {
 async def test_config_values_are_retained_after_pod_upgraded(ops_test, grafana_charm):
     """Deploy from charmhub and then upgrade with the charm-under-test."""
     logger.info("deploy charm from charmhub")
-    await ops_test.model.deploy(
-        f"ch:{app_name}", application_name=app_name, channel="edge", trust=True
-    )
+    sh.juju.deploy(app_name, model=ops_test.model.name, channel="2/edge", trust=True)
 
     # set some custom configs to later check they persisted across the test
     config = {"log_level": "error", "admin_user": "jimmy"}
+    await ops_test.model.wait_for_idle(apps=[app_name], status="active", timeout=1000)
     await ops_test.model.applications[app_name].set_config(config)
     await ops_test.model.wait_for_idle(apps=[app_name], status="active", timeout=1000)
 
     logger.info("upgrade deployed charm with local charm %s", grafana_charm)
-    await ops_test.model.applications[app_name].refresh(
-        path=grafana_charm, resources=grafana_resources
+    sh.juju.refresh(
+        app_name,
+        model=ops_test.model.name,
+        path=grafana_charm,
+        resource=[f"{k}={v}" for k, v in grafana_resources.items()],
     )
     await ops_test.model.wait_for_idle(apps=[app_name], status="active", timeout=1000)
     await check_grafana_is_ready(ops_test, app_name, 0)
